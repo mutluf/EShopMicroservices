938c135a5453b424752ef08e031ac30412072231
Fatma <fmutlu3002@gmail.com>
2023-08-28 15:00:36 +0300

    Integration event and subscription manager

---
+++ src/BuildingBlocks/EventBus/EventBus.Base/Abstraction/IEventBus.cs
@@ -0,0 +1,12 @@
+﻿using EventBus.Base.Events;
+
+namespace EventBus.Base.Abstraction
+{
+    public interface IEventBus
+    {
+        void Publish(IntegrationEvent @event);
+        void Subscribe<TEvent, THandler>() where TEvent : IntegrationEvent where THandler : IIntegrationEventHandler<TEvent>;
+
+        void Unsubscribe<TEvent, THandler>() where TEvent : IntegrationEvent where THandler: IIntegrationEventHandler<TEvent>;
+    }
+}
