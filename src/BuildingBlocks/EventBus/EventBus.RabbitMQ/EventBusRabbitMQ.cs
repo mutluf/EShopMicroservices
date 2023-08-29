6822f222babd1a9031650601abbc408ab38ef0d5
Fatma <fmutlu3002@gmail.com>
2023-08-29 18:48:05 +0300

    RabbitMQ implementation

---
+++ src/BuildingBlocks/EventBus/EventBus.RabbitMQ/EventBusRabbitMQ.cs
@@ -0,0 +1,34 @@
+﻿using EventBus.Base;
+using EventBus.Base.Abstraction;
+using EventBus.Base.Events;
+
+namespace EventBus.RabbitMQ
+{
+    public class EventBusRabbitMQ : BaseEventBus
+    {
+        public EventBusRabbitMQ(IServiceProvider serviceProvider, IEventBusSubscriptionManager subscriptionManager, EventBusConfiguration eventBusConfig) : base(serviceProvider, subscriptionManager, eventBusConfig)
+        {
+        }
+
+        public override void Publish(IntegrationEvent @event)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Subscribe<TEvent, THandler>()
+        {
+            var eventName = typeof(TEvent).Name;    
+            eventName = ProcessEventName(eventName);
+            
+            if(!_subscriptionManager.HasSubscriptionForEvent(eventName))
+            {
+
+            }
+        }
+
+        public override void Unsubscribe<TEvent, THandler>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
