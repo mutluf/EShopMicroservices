28c71838528012d1744eb3aa3c86e431f0f6a497
Fatma <fmutlu3002@gmail.com>
2023-08-30 21:51:32 +0300

    Catalog service

---
+++ src/Services/CatalogService/CatalogService.Api/Infrastructure/Context/CatalogContextSeed.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Data.SqlClient;
+using Polly;
+using System.IO.Compression;
+
+namespace CatalogService.Api.Infrastructure.Context
+{
+    public class CatalogContextSeed
+    {
+        public async Task SeedAsync(CatalogContext context, IWebHostEnvironment env, ILogger<CatalogContextSeed> _logger)
+        {
+
+            var policy = Policy.Handle<SqlException>()
+                .WaitAndRetryAsync(
+                retryCount: 3,
+                sleepDurationProvider: retry => TimeSpan.FromMicroseconds(5),
+                onRetry: (exception, timeSpan, retry, ctx) =>
+                {
+                    _logger.LogWarning(exception, "[{prefix}] Exception {ExceptionType} with message {Message} detected on attempt {retry} of {retryCount}");
+                });
+
+            var setupDirParth = Path.Combine(env.ContentRootPath, "Infrastructure", "Setup", "SeedFiles");
+            var picturePath = "Pics";
+
+            await policy.ExecuteAsync(() =>  ProcessSeeding(context, setupDirParth, picturePath, _logger)
+            );
+        }
+
+        private async Task ProcessSeeding(CatalogContext context, string setupDirParth, string picturePath, ILogger logger)
+        {
+            if(!context.CatalogBrands.Any())
+            {
+                await context.CatalogBrands.AddRangeAsync();
+                await context.SaveChangesAsync();
+            }
+
+            if (!context.CatalogItems.Any())
+            {
+                await context.CatalogItems.AddRangeAsync();
+                await context.SaveChangesAsync();
+            }
+
+            if (!context.CatalogTypes.Any())
+            {
+                await context.CatalogTypes.AddRangeAsync();
+                await context.SaveChangesAsync();
+            }
+        }
+
+
+        private void GetCatalogItemPictures(string contentPath, string picturePath)
+        {
+            picturePath ??= "pics";
+
+            if (picturePath != null)
+            {
+                DirectoryInfo directory = new DirectoryInfo(picturePath);
+                foreach (FileInfo file in directory.GetFiles())
+                {
+                    file.Delete();
+                }
+
+                string zipFileCatalogItemPictures = Path.Combine(contentPath, "CatalogItems.zip");
+                ZipFile.ExtractToDirectory(zipFileCatalogItemPictures, picturePath);
+            }
+        }
+    }
+}
